08195c8a5f3690ad6d8c818abdbe22d77dcfa371
HugoRedon <hugo_redon_@hotmail.com>
2014-10-26 21:59:47 -0500

    pipeline added

--- src/main/webapp/resources/js/hrr/plant.js
+++ src/main/webapp/resources/js/hrr/plant.js
@@ -1,19 +1,23 @@
 function Plant (scene) {
 	this.objects=[];
 	this.scene;	
-	
+	this.tankGeometry;
+	this.heatExchangerGeometry;
+	this.towerGeometry;
+	this.material = new THREE.MeshPhongMaterial( { 
+											        color: 0x444444, 
+											        ambient: 0x444444, // should generally match color
+											        specular: 0x050505,
+											        side: THREE.DoubleSide,
+											        shininess: 100
+											    } );  
+								
    
        var parent;
        
 
        function addGeometry( geometry, color, x, y, z, rx, ry, rz, s ) {
-         var mesh = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { 
-	            color: 0x444444, 
-	            ambient: 0x444444, // should generally match color
-	            specular: 0x050505,
-	            side: THREE.DoubleSide,
-	            shininess: 100
-	        } )  );
+         var mesh = new THREE.Mesh( geometry, plant.material );
          mesh.castShadow = true;
       
 
@@ -30,70 +34,52 @@ function Plant (scene) {
 
        }
 	
-	this.smiley=function(){
-		
-		   var randomPoints = [];
 
-	        for (var i=0; i<10;i++) {
-	          randomPoints.push(
-	            new THREE.Vector3(Math.random() * 200,Math.random() * 200,Math.random() * 200 )
-	            );
-	        }
-		var extrudeSettings = { amount: 200,  bevelEnabled: true, bevelSegments: 2, steps: 150 };
-		var randomSpline =  new THREE.SplineCurve3(randomPoints);
-
-        extrudeSettings.extrudePath = randomSpline; 
-		 
 	
-	      
-	        var tube = new THREE.TubeGeometry(extrudeSettings.extrudePath, 150, 4, 5, false, true);
-	        // new THREE.TubeGeometry(extrudePath, segments, 2, radiusSegments, closed2, debug);	      	
-	        addGeometry( tube, 0x00ff11,  0,  0, 0,     0, 0, 0, 1 );  
-	        console.log(tube);
+
+	
+	
+	this.addTank = function(){
+		createMeshAndAdd( this.tankGeometry,"Tanque esférico");			
+	}
+	this.addHeatExchanger = function(){
+		return createMeshAndAdd(this.heatExchangerGeometry,"Intercambiador de calor");
 	};
 	
+	this.addTower= function(){
+		return createMeshAndAdd(this.towerGeometry,"Torre de destilación");
+	}
 	
-
-	this.load3dModel = function(modelPath,nameInfo,index){
-		
-		console.log("start loading");
-		var loader = new THREE.JSONLoader();
-        loader.load( modelPath, function( geometry ) {
-        	variable = new THREE.Mesh( geometry, new THREE.MeshPhongMaterial( { 
-	            color: 0x444444, 
-	            ambient: 0x444444, // should generally match color
-	            specular: 0x050505,
-	            side: THREE.DoubleSide,
-	            shininess: 100
-	        } )  );
-        	variable.scale.set( 20, 20, 20 );
-        	//variable.position.z=25;
-        	variable.position.y = Math.random()*500;
-        	variable.position.x = Math.random()*500;
-        	variable.rotation.x = Math.PI/2;
-        	variable.castShadow = true;
-        	variable.receiveShadow = true;
-	    	variable.userData={name:nameInfo};
-	        scene.add(variable);
-	        plant.objects[index]=variable;
-	        //plant.drawTubeBetween(variable,{position:{x:Math.random()*500,y:Math.random()*500,z:125}});
-	       plant.drawTubes();
-        });
-        
-        return heatExchanger;
-	};
+	function createMeshAndAdd(geometry,info){
+		var mesh = new THREE.Mesh( geometry,plant.material.clone());
+		mesh.scale.set(20,20,20);
+		mesh.position.y = Math.random()*500;
+		mesh.position.x = Math.random()*500;
+		mesh.rotation.x = Math.PI/2;
+		mesh.castShadow=true;
+		mesh.receiveShadow=true;
+		mesh.userData={name:info};
+		scene.add(mesh);
+		plant.objects.push(mesh);
+		return mesh;
+	}
 	
 	this.drawTubes=function(){
 		scene.remove(parent);
 		parent = new THREE.Object3D();
 	   scene.add( parent );
 		
-		if(plant.objects.length == 3){
-			for(var i = 0; i < plant.objects.length-1;i++){
-				var ob1 = plant.objects[i];
-				var ob2 = plant.objects[i+1];
-				this.drawTubeBetween(ob1,ob2);
+		
+		for(var i = 0; i < plant.objects.length ;i++){
+			var obj = plant.objects[i];
+			if(obj.nextEquipment){
+				var next = obj.nextEquipment;
+				this.drawTubeBetween(obj,next);
 			}
+//				var ob1 = plant.objects[i];
+//				var ob2 = plant.objects[i+1];
+//				this.drawTubeBetween(ob1,ob2);
+			
 		}
 	}
 	this.drawTubeBetween= function(o1,o2){
@@ -137,17 +123,46 @@ function Plant (scene) {
 	var loading = true;
 	
 	
-	this.loadModels= function(){
+	this.loadGeometry = function(modelPath){
 		
-		this.load3dModel("models/sphericalTank.txt","Tanque esférico",0);
-		this.load3dModel("models/simple-heat-exchanger.txt","Intercambiador de calor",1);
-		this.load3dModel("models/towerModel.txt","Torre de destilación",2);
 		
-
-		//this.smiley();
 	};
-	this.loadModels();
 	
+	this.loadGeometries= function(){
+		var loader = new THREE.JSONLoader();
+		loader.load( "models/sphericalTank.txt", function( geometry ){
+			plant.tankGeometry = geometry;				
+		});
+
+		loader.load( "models/simple-heat-exchanger.txt", function( geometry ){
+			plant.heatExchangerGeometry = geometry;				
+		});
+		loader.load( "models/towerModel.txt", function( geometry ){
+			plant.towerGeometry = geometry;				
+		});
+		
+	
+	};
+	this.loadGeometries();
+	
+	
+	this.remove = function(toRemove){
+		var index = plant.objects.indexOf(toRemove);
+		if(index > -1){
+			plant.objects.splice(index,1);	
+		}
+		
+		
+		if(toRemove.previousEquipment){
+			toRemove.previousEquipment.nextEquipment = null;
+		}
+		if(toRemove.nextEquipment){
+			plant.remove(toRemove.nextEquipment);
+		}
+		
+		scene.remove(toRemove);
+		plant.drawTubes();
+	};
 	
 	
 	
